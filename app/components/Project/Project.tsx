104bb2cf82f1f66e4d2bb199ffa87af2acd6a325
Ayush <ayushchahat110903@gmail.com>
2024-12-28 19:26:09 +0530

    Made changes in link for projects

--- app/components/Project/Project.tsx
+++ app/components/Project/Project.tsx
@@ -5,28 +5,28 @@ import styles from "../../styles/Project.module.css";
 const Project = () => {
   const projects = [
     {
+      title: "Smart Agro",
+      description:
+        "An IoT-based smart agriculture system for monitoring and controlling farm conditions.",
+      repoLink: "https://github.com/ayushchahat/Smart_Agro",
+      demoLink: "https://smart-agro.com",
+      technologies: ["Node.js", "Express", "MongoDB", "IoT"],
+    },
+    {
       title: "GoFoodie",
       description:
         "A full-stack application for food delivery, providing real-time order tracking and notifications.",
-      repoLink: "https://github.com/ayushchahat/GoFoodie",
-      demoLink: "https://gofoodie.com",
+      repoLink: "https://github.com/ayushchahat/Gofood",
+      demoLink: "https://gofood-frontend-my1b.onrender.com/",
       technologies: ["React", "Node.js", "MongoDB"],
     },
     {
       title: "Chat-Now",
       description:
         "A real-time messaging app with support for one-on-one and group chats.",
-      repoLink: "https://github.com/ayushchahat/Chat-Now",
-      demoLink: "https://chat-now.com",
+      repoLink: "https://github.com/ayushchahat/Chat_Bot",
+      demoLink: "https://chat-now-7mbr.onrender.com/",
       technologies: ["React.js", "Socket.IO", "Node.js"],
-    },
-    {
-      title: "Smart Agro",
-      description:
-        "An IoT-based smart agriculture system for monitoring and controlling farm conditions.",
-      repoLink: "https://github.com/ayushchahat/Smart-Agro",
-      demoLink: "https://smart-agro.com",
-      technologies: ["Node.js", "Express", "MongoDB", "IoT"],
     },
   ];
 
